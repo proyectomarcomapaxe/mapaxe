dbcdff9de24dc9e2c561f168fbb15f3e2b73b2cc
Marco mapaxe <proyectomapaxe@gmail.com>
2015-01-26 22:26:24 +0100

    Documentation updated

--- doc/elementlist.js
+++ doc/elementlist.js
@@ -1,3 +1,3 @@
 
 var ApiGen = ApiGen || {};
-ApiGen.elements = [["c","BadFunctionCallException"],["c","Exception"],["f","extime()"],["c","InvalidArgumentException"],["c","LogicException"],["c","mapaxe\\core\\Marco"],["c","mapaxe\\libs\\Helper"],["f","require_once_all()"],["c","RuntimeException"],["c","stdClass"]];
+ApiGen.elements = [["c","BadFunctionCallException"],["c","DOMNode"],["c","Exception"],["f","extime()"],["c","InvalidArgumentException"],["c","LogicException"],["c","mapaxe\\core\\Config"],["c","mapaxe\\core\\Handler"],["c","mapaxe\\core\\Marco"],["c","mapaxe\\libs\\Helper"],["c","mapaxe\\libs\\Log"],["c","mapaxe\\libs\\Xml"],["f","require_once_all()"],["c","RuntimeException"],["c","stdClass"]];
