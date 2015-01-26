b2a5af4c077356dd410a5ddb2a4d96ff15a99cd0
Marco mapaxe <proyectomapaxe@gmail.com>
2015-01-26 22:28:51 +0100

    Init of the core

--- core/entry.php
+++ core/entry.php
@@ -1,6 +1,15 @@
 <?php
+//declaration of package
 namespace mapaxe\core;
+if( !defined('ENTRYPOINT') )	die('Restricted Access');
+
+//importing function to use
 use function mapaxe\libs\require_once_all;
 
 //importing all files in this directory and subfolder
 require_once_all( dirname(__FILE__) );
+
+$config=new \stdClass();
+$config->filename=dirname(dirname(__FILE__)).DIRECTORY_SEPARATOR.'config.xml';
+
+$mapaxe=Marco::get_instance($config);
